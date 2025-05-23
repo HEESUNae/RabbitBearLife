f1c7e8d0be5015bc84cd024a02f4646dcedcfe02
HEESUNae <heesun9456@gmail.com>
2025-05-24 01:46:45 +0900

    🔵 other: pwa 배포 테스트

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
+const nextConfig: NextConfig = withPWA({
   /* config options here */
   images: {
     domains: ['res.cloudinary.com'],
@@ -10,6 +16,6 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '10mb', // 최대 10MB로 늘림
     },
   },
-};
+});
 
 export default nextConfig;
